6c0c63a1863b2342703ae3656fdd9237052e837f
Quentin SCHROTER <qschroter@gpi-experts.com>
2019-11-14 20:49:58 +0100

    add AndAlso extension

--- Bones/ExpressionBuilder.cs
+++ Bones/ExpressionBuilder.cs
@@ -12,16 +12,16 @@ namespace Bones
             Expression exp = null;
             ParameterExpression pe = Expression.Parameter(typeof(TSource), "e");
 
-            Expression replaced = new SwapVisitor(comparatorAccessor.Parameters[0],pe).Visit(comparatorAccessor.Body);
+            Expression replaced = new SwapVisitor(comparatorAccessor.Parameters[0], pe).Visit(comparatorAccessor.Body);
 
             foreach (var item in items)
             {
                 ConstantExpression ce = Expression.Constant(item);
                 BinaryExpression be = Expression.Equal(replaced, ce);
 
-                exp = exp == null? be : Expression.Or(exp, be);
+                exp = exp == null ? be : Expression.Or(exp, be);
             }
-            
+
             var result = Expression.Lambda<Func<TSource, bool>>(exp, new[] { pe });
             return result;
         }
@@ -31,18 +31,32 @@ namespace Bones
             Expression exp = null;
             ParameterExpression pe = Expression.Parameter(typeof(TSource), "e");
 
-            Expression replaced = new SwapVisitor(comparison.Parameters[0],pe).Visit(comparison.Body);
+            Expression replaced = new SwapVisitor(comparison.Parameters[0], pe).Visit(comparison.Body);
 
             foreach (var item in items)
             {
                 ConstantExpression ce = Expression.Constant(item);
                 Expression inserted = new SwapVisitor(comparison.Parameters[1], ce).Visit(replaced);
 
-                exp = exp == null? inserted : Expression.Or(exp, inserted);
+                exp = exp == null ? inserted : Expression.Or(exp, inserted);
             }
 
             var result = Expression.Lambda<Func<TSource, bool>>(exp, new[] { pe });
             return result;
+        }
+
+        public static Expression<Func<TSource, bool>> AndAlso<TSource>(this Expression<Func<TSource, bool>> expr1, Expression<Func<TSource, bool>> expr2)
+        {
+            var parameter = Expression.Parameter(typeof(TSource), "d");
+
+            var leftVisitor = new SwapVisitor(expr1.Parameters[0], parameter);
+            var left = leftVisitor.Visit(expr1.Body);
+
+            var rightVisitor = new SwapVisitor(expr2.Parameters[0], parameter);
+            var right = rightVisitor.Visit(expr2.Body);
+
+            return Expression.Lambda<Func<TSource, bool>>(
+                Expression.AndAlso(left, right), parameter);
         }
 
     }
