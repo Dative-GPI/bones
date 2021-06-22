2e7faf0cc142656d78753ab14225c76022352cff
qschroter <qschroter@gpi-experts.com>
2021-06-22 17:01:42 +0200

    add extension for distinctBy

--- src/Bones/Extensions/EnumerableExtensions.cs
+++ src/Bones/Extensions/EnumerableExtensions.cs
@@ -1,16 +1,33 @@
+using System;
 using System.Collections.Generic;
 
-public static class EnumerableExtensions
+namespace System.Linq
 {
-    /// <summary>
-    /// Wraps this object instance into an IEnumerable&lt;T&gt;
-    /// consisting of a single item.
-    /// </summary>
-    /// <typeparam name="T"> Type of the object. </typeparam>
-    /// <param name="item"> The instance that will be wrapped. </param>
-    /// <returns> An IEnumerable&lt;T&gt; consisting of a single item. </returns>
-    public static IEnumerable<T> Yield<T>(this T item)
+    public static class EnumerableExtensions
     {
-        yield return item;
+        /// <summary>
+        /// Wraps this object instance into an IEnumerable&lt;T&gt;
+        /// consisting of a single item.
+        /// </summary>
+        /// <typeparam name="T"> Type of the object. </typeparam>
+        /// <param name="item"> The instance that will be wrapped. </param>
+        /// <returns> An IEnumerable&lt;T&gt; consisting of a single item. </returns>
+        public static IEnumerable<T> Yield<T>(this T item)
+        {
+            yield return item;
+        }
+
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
     }
 }
+
