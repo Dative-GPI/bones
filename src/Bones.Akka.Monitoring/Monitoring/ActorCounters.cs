16b16f33fb23b2db7d309bea9d78704bfba77a48
gballigand <gballigand@dative-gpi.com>
2023-01-30 08:34:45 +0100

    counters improvment

--- src/Bones.Akka.Monitoring/Monitoring/ActorCounters.cs
+++ src/Bones.Akka.Monitoring/Monitoring/ActorCounters.cs
@@ -6,72 +6,69 @@ namespace Bones.Akka.Monitoring
 {
     public class ActorCounters
     {
-        private Meter _akkaMeter;
-        private KeyValuePair<string, object> actorPathTag;
-        private KeyValuePair<string, object> actorTypeTag;
-        private Counter<int> receivedMessagesCounter;
-        private Counter<int> createdActorCounter;
-        private Counter<int> restartedActorCounter;
-        private Counter<int> stoppedActorCounter;
-        private Counter<int> unhandledMessagesCounter;
-        private ObservableGauge<int> messageQueueCounter;
-        private Histogram<long> messagesLatencyHistogram;
-        private Counter<int> logsCounter; //Not implemented yet
-        private Counter<int> deadLettersCounter; //Not implemented yet
-        private int currentNumberOfMessages { get; set; }
+        private KeyValuePair<string, object> _actorPathTag;
+        private KeyValuePair<string, object> _actorTypeTag;
+        private Counter<int> _receivedMessagesCounter;
+        private Counter<int> _createdActorCounter;
+        private Counter<int> _restartedActorCounter;
+        private Counter<int> _stoppedActorCounter;
+        private Counter<int> _unhandledMessagesCounter;
+        private ObservableGauge<int> _messageQueueCounter;
+        private Histogram<long> _messagesLatencyHistogram;
+        // private Counter<int> _logsCounter; //Not implemented yet
+        // private Counter<int> _deadLettersCounter; //Not implemented yet
+        private int _currentNumberOfMessages;
         
         public ActorCounters(IActorContext context, Meter meter)
         {
-            actorTypeTag = new KeyValuePair<string, object>(AkkaMetricsNames.ACTOR_TYPE_LABEL, context.Props.TypeName);
-            actorPathTag = new KeyValuePair<string, object>(AkkaMetricsNames.ACTOR_PATH_LABEL, context.Self.Path.ToString());
+            _actorTypeTag = new KeyValuePair<string, object>(AkkaMetricsNames.ACTOR_TYPE_LABEL, context.Props.TypeName);
+            _actorPathTag = new KeyValuePair<string, object>(AkkaMetricsNames.ACTOR_PATH_LABEL, context.Self.Path.ToString());
 
-            receivedMessagesCounter = meter.CreateCounter<int>(AkkaMetricsNames.RECEIVED_MESSAGE_COUNTER, description: "count the number of messages received by each message type / actor type pair.");
-            createdActorCounter = meter.CreateCounter<int>(AkkaMetricsNames.CREATED_ACTOR_COUNTER, description: "counts the number of actor restarts over time.");
-            restartedActorCounter = meter.CreateCounter<int>(AkkaMetricsNames.RESTARTED_ACTOR_COUNTER, description: "counts the number of actors started.");
-            stoppedActorCounter = meter.CreateCounter<int>(AkkaMetricsNames.STOPPED_ACTOR_COUNTER, description: "counts the number of actors stopped.");
-            messageQueueCounter = meter.CreateObservableGauge<int>(AkkaMetricsNames.MAILBOX_GAUGE, 
-                () => new Measurement<int>(this.currentNumberOfMessages, actorTypeTag, actorPathTag), description: "a gauge that measures the queue length of the actor. Should be enabled");
-            unhandledMessagesCounter = meter.CreateCounter<int>(AkkaMetricsNames.UNHANDLED_MESSAGE_COUNTER, description: "counts the number of Unhandled messages by message type.");
-            messagesLatencyHistogram = meter.CreateHistogram<long>(AkkaMetricsNames.MESSAGE_LATENCY_HISTOGRAM, description: "a timer that messages the latency for each message / actor type pair; uses the same end to end measurement that tracing does.");
+            _receivedMessagesCounter = meter.CreateCounter<int>(AkkaMetricsNames.RECEIVED_MESSAGE_COUNTER, description: "count the number of messages received by each message type / actor type pair.");
+            _createdActorCounter = meter.CreateCounter<int>(AkkaMetricsNames.CREATED_ACTOR_COUNTER, description: "counts the number of actor restarts over time.");
+            _restartedActorCounter = meter.CreateCounter<int>(AkkaMetricsNames.RESTARTED_ACTOR_COUNTER, description: "counts the number of actors started.");
+            _stoppedActorCounter = meter.CreateCounter<int>(AkkaMetricsNames.STOPPED_ACTOR_COUNTER, description: "counts the number of actors stopped.");
+            _messageQueueCounter = meter.CreateObservableGauge<int>(AkkaMetricsNames.MAILBOX_GAUGE, 
+                () => new Measurement<int>(this._currentNumberOfMessages, _actorTypeTag, _actorPathTag), description: "a gauge that measures the queue length of the actor. Should be enabled");
+            _unhandledMessagesCounter = meter.CreateCounter<int>(AkkaMetricsNames.UNHANDLED_MESSAGE_COUNTER, description: "counts the number of Unhandled messages by message type.");
+            _messagesLatencyHistogram = meter.CreateHistogram<long>(AkkaMetricsNames.MESSAGE_LATENCY_HISTOGRAM, description: "a timer that messages the latency for each message / actor type pair; uses the same end to end measurement that tracing does.");
             // logsCounter = _akkaMeter.CreateCounter<int>("akka.logs", description: "counts the number of logs of each log level into a collection. Also collections the Exception types raised in the error logs");
             // deadLettersCounter = _akkaMeter.CreateCounter<int>("akka.messages.deadletters", description: "counts the number of DeadLetter messages by message type.");
         }
 
         public void UpdateMessageQueueCounter(int numberOfMessages)
         {
-            this.currentNumberOfMessages = numberOfMessages;
+            this._currentNumberOfMessages = numberOfMessages;
         }
 
         public void IncrementCreatedActorsCounter()
         {
-            receivedMessagesCounter.Add(1, actorTypeTag, actorPathTag);
+            _receivedMessagesCounter.Add(1, _actorTypeTag, _actorPathTag);
         }
 
         public void IncrementMessagesCounter(KeyValuePair<string, object> messageTag)
         {
-            receivedMessagesCounter.Add(1, actorTypeTag, actorPathTag, messageTag);
+            _receivedMessagesCounter.Add(1, _actorTypeTag, _actorPathTag, messageTag);
         }
 
         public void RecordMessageLatency(long latency, KeyValuePair<string, object> messageType)
         {
-            messagesLatencyHistogram.Record(latency, actorTypeTag, actorPathTag, messageType);
+            _messagesLatencyHistogram.Record(latency, _actorTypeTag, _actorPathTag, messageType);
         }
 
         public void IncrementRestartedActorsCounter(KeyValuePair<string, object> exceptionTypeTag)
         {
-            restartedActorCounter.Add(1, actorTypeTag, actorPathTag, exceptionTypeTag);
+            _restartedActorCounter.Add(1, _actorTypeTag, _actorPathTag, exceptionTypeTag);
         }
 
         public void IncrementStoppedActorsCounter()
         {
-            stoppedActorCounter.Add(1, actorTypeTag, actorPathTag);
+            _stoppedActorCounter.Add(1, _actorTypeTag, _actorPathTag);
         }
 
         public void IncrementUnhandledMessagesCounter(KeyValuePair<string, object> messageTag)
         {
-            unhandledMessagesCounter.Add(1, actorTypeTag, actorPathTag, messageTag);
+            _unhandledMessagesCounter.Add(1, _actorTypeTag, _actorPathTag, messageTag);
         }
     }
-
-
 }
