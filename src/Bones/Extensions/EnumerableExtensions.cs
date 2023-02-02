da9a15287f17bf8449c0d73748a517baedbab746
gballigand <gballigand@dative-gpi.com>
2023-02-02 10:15:36 +0100

    remove DistinctBy which is now integrated in .net7

--- src/Bones/Extensions/EnumerableExtensions.cs
+++ src/Bones/Extensions/EnumerableExtensions.cs
@@ -16,18 +16,6 @@ namespace System.Linq
         {
             yield return item;
         }
-
-        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
-        {
-            HashSet<TKey> seenKeys = new HashSet<TKey>();
-            foreach (TSource element in source)
-            {
-                if (seenKeys.Add(keySelector(element)))
-                {
-                    yield return element;
-                }
-            }
-        }
     }
 }
 
