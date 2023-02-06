6cbca19a391f00f784792de4778fe6049693b459
gballigand <gballigand@dative-gpi.com>
2023-02-06 14:59:57 +0100

    Labels improvement

--- src/Bones.Akka.Monitoring/Monitoring/AkkaMetricsNames.cs
+++ src/Bones.Akka.Monitoring/Monitoring/AkkaMetricsNames.cs
@@ -2,6 +2,7 @@ namespace Bones.Akka.Monitoring
 {
     public static class AkkaMetricsNames
     {
+        #region counter names
         public const string CREATED_ACTOR_COUNTER = "akka.actor.created";
         public const string RESTARTED_ACTOR_COUNTER = "akka.actor.restarted";
         public const string STOPPED_ACTOR_COUNTER = "akka.actor.stopped";
@@ -9,11 +10,14 @@ namespace Bones.Akka.Monitoring
         public const string UNHANDLED_MESSAGE_COUNTER = "akka.message.unhandled";
         public const string MESSAGE_LATENCY_HISTOGRAM = "akka.message.latency";
         public const string MAILBOX_GAUGE = "akka.mailbox";
+        #endregion
+
+        #region counters tags
         public const string ACTOR_TYPE_LABEL = "actor_type";
         public const string ACTOR_PATH_LABEL = "actor_path";
-        public const string MESSAGE_TYPE_LABEL = "actor_type";
+        public const string MESSAGE_TYPE_LABEL = "messsage_type";
         public const string SENDER_LABEL = "sender_path";
         public const string EXCEPTION_TYPE_LABEL = "exception_type";
-        public const string MESSAGE_ACTIVITY_LABEL = "akka.msg.recv";
+        #endregion
     }
 }
