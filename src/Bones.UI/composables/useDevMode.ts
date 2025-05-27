02afe4060119a0fb07d72cfe51b94933cc6b5493
qschroter <qschroter@gpi-experts.com>
2025-05-27 13:58:44 +0200

    clean with const parameter

--- src/Bones.UI/composables/useDevMode.ts
+++ src/Bones.UI/composables/useDevMode.ts
@@ -2,16 +2,17 @@ import { ref, computed } from "vue";
 
 import { EventQueue } from "../core/eventQueue";
 
+const DEV_MODE_TOPIC = 'devMode';
 const devMode = ref(false);
 
-EventQueue.instance.subscribe('devMode', (_topic: string, payload: boolean) => {
+EventQueue.instance.subscribe(DEV_MODE_TOPIC, (_topic: string, payload: boolean) => {
     devMode.value = payload;
     console.log(`Dev mode is now ${devMode.value ? 'enabled' : 'disabled'}`);
 });
 
 export function useDevMode() {
     const toggleDevMode = () => {
-        EventQueue.instance.publish('devMode', !devMode.value);
+        EventQueue.instance.publish(DEV_MODE_TOPIC, !devMode.value);
     };
 
     const isDevMode = computed(() => devMode.value);
